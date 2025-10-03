81061f2c65aa939e8e9bcba61a1afaffd5bd43d5
vansh76711112 <145377923+vansh76711112@users.noreply.github.com>
2025-10-03 18:10:21 +0530

    Update layout.js

--- app/(main)/resume/layout.js
+++ app/(main)/resume/layout.js
@@ -3,7 +3,7 @@ import { BarLoader } from 'react-spinners'
 
 const Layout = ({children}) => {
   return (
-    <div children="px-5">
+    <div className="px-5">
       <Suspense
       fallback={<BarLoader className='mt-4 width={"100%"} colors="gray"'/>}>
         {children}
